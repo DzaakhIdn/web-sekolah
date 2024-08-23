e68cbf28f2118272a50b0c6e9fd151394a455071
DzaakhIdn <dzaakhidn@gmail.com>
2024-08-23 17:11:31 +0700

    new edit 23/08

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 let hamburger = document.getElementById('hamburger')
 let nav = document.getElementById('nav')
+let toggle = document.querySelector(".toggle")
+
+toggle.addEventListener("click", function(){
+    console.log('jalan');
+    nav.classList.add("h-[20rem]")
+    nav.classList.remove("h-[0rem]")
+})
