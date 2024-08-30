0a812d73c6028965ea200185743386b9c0eda8ac
DzaakhIdn <dzaakhidn@gmail.com>
2024-08-30 16:59:27 +0700

    update afternoon 30/08

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '10px 10px 0px -3px rgba(140,199,250,1)',
+      }
+    },
   },
   plugins: [],
 }
