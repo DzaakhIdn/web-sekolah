a424ba385e61fbf60b37fe4ca3f58d4c401925bc
DzaakhIdn <dzaakhidn@gmail.com>
2024-09-04 17:41:34 +0700

    Update 4/09

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       boxShadow: {
         '3xl': '10px 10px 0px -3px rgba(140,199,250,1)',
+      },
+      gridAutoColumns: {
+        'grid-card' : 'calc((100% / 3) - 12px)'
       }
     },
   },
